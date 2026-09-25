d2fa61fce993cec0011ac38cf68abc5366c3a87f
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2026-09-25 19:37:32 +1000

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "script that prints the gradle version in the repository";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = import ./package.nix { inherit pkgs; };
+    });
+  };
+}
